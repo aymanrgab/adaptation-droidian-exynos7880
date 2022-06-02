c3517c74e3f380e7ac23ccbb3330b8353bcaa5f4
aymanrgab <aymanrar2c@gmail.com>
2022-06-02 17:58:39 +0200

    add udev rules, fix perm

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ umount /mnt/
 mount /data/rootfs.img /mnt/
 cd droidian
 cp -r * /mnt/
-chroot /mnt/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin" && systemctl enable samsung-hwc && dpkg -i /tmp/debs/*.deb && rm -rf /tmp/debs'
+chroot /mnt/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin" && systemctl enable samsung-hwc && chmod 755 /var/lib/lxc/android/pr* && dpkg -i /tmp/debs/*.deb && rm -rf /tmp/debs'
 umount /mnt/
 echo "Done!"
 echo "It is recommended to run these command once Droidian is booted;"
